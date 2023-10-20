ad2c3f77ee8fe6d51360b5aaa47c056aa7480d6c
Thor Lindberg <github@thor.computer>
2023-10-20 10:38:39 +0200

    Pins no longer appear until their image is loaded

--- app/components/Pin/Pin.tsx
+++ app/components/Pin/Pin.tsx
@@ -4,7 +4,7 @@ import useStyle from './styles';
 import PinProps from './types';
 import useDestinationHandler from '../../handlers/destinationHandler';
 import Rounded from '../Rounded/Rounded';
-import {ActivityIndicator, Image, View} from 'react-native';
+import {Image, View} from 'react-native';
 
 const Pin = ({location}: PinProps) => {
   const styles = useStyle();
@@ -12,6 +12,7 @@ const Pin = ({location}: PinProps) => {
   const destinationHandler = useDestinationHandler({location});
   return (
     <Marker
+      style={{opacity: isLoaded ? 1 : 0}}
       key={location.url}
       onPress={destinationHandler}
       tracksViewChanges={!isLoaded}
@@ -21,7 +22,6 @@ const Pin = ({location}: PinProps) => {
       }}>
       <Rounded radius={1000} smooth style={styles.pinStyle}>
         <View style={styles.pinStyle}>
-          {!isLoaded && <ActivityIndicator size="small" color={'black'} />}
           <Image
             source={{uri: location.image}}
             style={{...styles.imageStyle, opacity: isLoaded ? 1 : 0}}
