d4e5e43b6a077d938f0bb45e83a525541fa64dfb
Thor Lindberg <github@thor.computer>
2023-10-20 11:45:01 +0200

    Update welcomeHandler.tsx

--- app/handlers/welcomeHandler.tsx
+++ app/handlers/welcomeHandler.tsx
@@ -3,7 +3,7 @@ import Welcome from '../screens/Welcome';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {useModal} from 'react-native-modal-provider';
 
-const APP_OPENED_KEY = 'appOpasdasened';
+const APP_OPENED_KEY = 'appOpened';
 
 const useWelcomeHandler = () => {
   const {openModal} = useModal();
