7e0d74e252211f1dc19bf5ae9d99c3c16117dd87
Thor <71700400+thorlindberg@users.noreply.github.com>
2024-02-25 17:41:40 +0100

    Added changelog to About

--- app/screens/About.tsx
+++ app/screens/About.tsx
@@ -5,16 +5,15 @@ import Container from '../components/Container/Container';
 import {useSelector} from 'react-redux';
 import {RootState} from '../store/store';
 import Rounded from '../components/Rounded/Rounded';
-import {Image} from 'react-native';
-import AppArt from '../components/AppArt/AppArt';
+import {Image, View} from 'react-native';
 import TitleBar from '../components/TitleBar/TitleBar';
 import browserHandler from '../handlers/browserHandler';
 import {useCallback} from 'react';
 import {useModal} from 'react-native-modal-provider';
+import {LinearGradient} from 'react-native-linear-gradient';
+import {BlurView} from '@react-native-community/blur';
 
 const iconURL = require('../assets/images/appicon.png');
-const artURL = require('../assets/images/IMG_7671.jpg');
-const icon = require('../assets/images/appicon.png');
 
 const About = () => {
   const {closeModal} = useModal();
@@ -37,34 +36,73 @@ const About = () => {
       confirmationAction={openAppStoreReview}
       titleColor="black"
       titleText={locationsData.app.name}
-      icon={icon}
       detent="medium">
       <Container scrollable gap="xlarge" padding={false}>
         <Container gap="xlarge">
           <Container padding={false} alignment="center">
-            <Rounded
-              radius={16}
-              smooth
-              style={{
-                alignItems: 'center',
-                justifyContent: 'center',
-                width: 72,
-                height: 72,
-              }}
-              borderWidth={1}
-              borderColor="white">
-              <Image
-                source={iconURL}
-                style={{
-                  width: 72,
-                  height: 72,
-                  position: 'absolute',
-                  backgroundColor: theme.primaryColor,
-                }}
-                resizeMode="cover"
-              />
-            </Rounded>
             <Container padding={false} alignment="center" gap="small">
+              <View style={{width: 102, height: 102, marginLeft: 30}}>
+                <Rounded
+                  radius={16}
+                  smooth
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: 72,
+                    height: 72,
+                    paddingTop: 10,
+                  }}
+                  borderWidth={1}
+                  borderColor="white">
+                  <LinearGradient
+                    colors={['#DC1E35', '#02529C']}
+                    start={{x: 0, y: 0.6}}
+                    end={{x: 1, y: 0.4}}
+                    style={{
+                      width: '100%',
+                      height: '100%',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                      opacity: 1 / 3,
+                    }}
+                  />
+                </Rounded>
+                <BlurView
+                  style={{
+                    position: 'absolute',
+                    top: -30,
+                    left: -30,
+                    bottom: 0,
+                    right: 0,
+                  }}
+                  blurType="light"
+                  blurAmount={15}
+                  reducedTransparencyFallbackColor="white"
+                />
+                <Rounded
+                  radius={16}
+                  smooth
+                  style={{
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: 72,
+                    height: 72,
+                    position: 'absolute',
+                  }}
+                  borderWidth={1}
+                  borderColor="white">
+                  <Image
+                    source={iconURL}
+                    style={{
+                      width: 72,
+                      height: 72,
+                      position: 'absolute',
+                      backgroundColor: theme.primaryColor,
+                    }}
+                    resizeMode="cover"
+                  />
+                </Rounded>
+              </View>
               <Text size="title" bold color={theme.primaryColor}>
                 {locationsData.app.name}
               </Text>
@@ -73,9 +111,40 @@ const About = () => {
               </Text>
             </Container>
           </Container>
-          <Text color={theme.primaryColor}>{locationsData.app.legal}</Text>
+          <Container padding={false} edges={['bottom']}>
+            <Text color={theme.primaryColor}>{locationsData.app.legal}</Text>
+            <Container padding={false}>
+              <Text size="large" bold color={theme.primaryColor}>
+                Changelog
+              </Text>
+              <Container padding={false} direction="row">
+                <View
+                  style={{
+                    backgroundColor: 'rgb(230, 230, 230)',
+                    width: 4,
+                    height: '100%',
+                  }}
+                />
+                <Container padding={false}>
+                  {Object.entries(locationsData.app.changelog).map(
+                    ([version, changes]) => (
+                      <Container padding={false} key={version} gap="small">
+                        <Text bold color={theme.primaryColor}>
+                          Version {version}
+                        </Text>
+                        {changes.map((change, index) => (
+                          <Text key={index} color={theme.primaryColor}>
+                            {'  '}•{'  '} {change}
+                          </Text>
+                        ))}
+                      </Container>
+                    ),
+                  )}
+                </Container>
+              </Container>
+            </Container>
+          </Container>
         </Container>
-        <AppArt source={artURL} />
       </Container>
     </TitleBar>
   );
